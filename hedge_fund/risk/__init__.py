4bebde341e86e79a749a56c9f6119da8f9deaa79
AlexAIG255 <alexkong255123@gmail.com>
2026-09-14 17:39:57 +0800

    Update __init__.py

--- hedge_fund/risk/__init__.py
+++ hedge_fund/risk/__init__.py
@@ -3,6 +3,6 @@
 Later: drawdown controls, volatility-based sizing, correlation caps.
 """
 
-from hedge_fund.risk.limits import ClampEvent, RiskLimits, RiskResult, apply_limits
+from .limits import ClampEvent, RiskLimits, RiskResult, apply_limits
 
 __all__ = ["ClampEvent", "RiskLimits", "RiskResult", "apply_limits"]
